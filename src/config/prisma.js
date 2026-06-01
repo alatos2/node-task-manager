1a6871e6a3d3e0388ff36347a05bad55529650ee
alatos2 <alabitosin60@gmail.com>
2026-06-01 22:17:19 +0000

    adjustments

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require('../generated/prisma/client')
 const { PrismaMariaDb } = require('@prisma/adapter-mariadb')
 
 const adapter = new PrismaMariaDb({
